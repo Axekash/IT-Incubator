481148fe64070f4baf077c752a7ea4b1be448b9a
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-03 19:07:04 +0300

    feat: add task #71.1

--- index.js
+++ index.js
@@ -31,4 +31,19 @@ function testElse(val) {
 
   //71
 
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else if (val < 5) {
+      return "Smaller than 5";
+    } else {
+      return "Between 5 and 10";
+    }
+  
+  }
+  
+  testElseIf(7);
+
+  //72
+
   
