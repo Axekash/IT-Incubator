c96846a6d3525aa372b501c2a9ccac2f50a8ed87
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-04 17:04:17 +0300

    feat: add js task 84

--- index.js
+++ index.js
@@ -351,6 +351,19 @@ const hatValue = testObj.hat;      // Change this line
 const shirtValue = testObj.shirt;    // Change this line
 
 //84
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj['an entree'];   // Change this line
+const drinkValue = testObj['the drink'];    // Change this line
+
+//85
+
 
 
 
