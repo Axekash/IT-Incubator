db739393e8410ff0f0c2b3fdda447a711b07cbce
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-04 16:33:43 +0300

    feat: add js task 82

--- index.js
+++ index.js
@@ -326,5 +326,18 @@ if(count > 0) {
 cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 //82
+const myDog = {
+  // Only change code below this line
+  name: 'Bobby',
+  legs: 4,
+  tails: 1,
+  friends: ['Bethoven', 'Rex', 'Woodey']
+
+
+  // Only change code above this line
+};
+
+//83
+
 
 
