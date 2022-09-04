106a8279d1f9282e15c0b7b09e7f92f3efc40394
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-04 18:32:52 +0300

    feat: add js task 88

--- index.js
+++ index.js
@@ -400,6 +400,27 @@ delete myDog['tails'];
 // Only change code below this line
 
 //88
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+const lookup = {
+  'alpha': "Adams",
+  'bravo': 'Boston',
+  'charlie': 'Chicago',
+  'delta': 'Denver',
+  'echo': 'Easy',
+  'foxtrot': 'Frank'
+}
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+
+// 89
 
 
 
