7e0e9af8cb53193af4424e3368f3fb1621abf4d4
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-04 15:38:21 +0300

    feat: add js task 79

--- index.js
+++ index.js
@@ -260,3 +260,14 @@ function chainToSwitch(val) {
 chainToSwitch(7);
 
 //79 
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+//80
+
